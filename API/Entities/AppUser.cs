161380cf187abf0e1830bfe905c6e09cbb42a3e2
and2684 <aaaa6226@gmail.com>
2022-05-27 20:55:32 +0500

    Added angular frontend (lesson 3) Quick tips for myself in order to run app:
    cd API -> dotnet watch run
    cd client -> ng serve

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace API.Entities
 {
     public class AppUser
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string UserName { get; set; }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -5,10 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// EF framework core adding Dbcontext
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// CORS is a security mechanism that not allows to run our api from other sources 
+// (for example api on localhost:5001 cannot be run from localhost:4200 while we didn't set up CORS)
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));  
 
 app.UseAuthorization();
 
