cea38f3898a4b3367eb74f8943534cb79d9803fc
and2684 <aaaa6226@gmail.com>
2022-06-07 21:45:08 +0500

    Added angular routing, bootswatch theme and toastr

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,7 +13,9 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, 
+              private router: Router, 
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -31,12 +35,17 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         console.log(response);
+        this.router.navigateByUrl('/members');
       },
-      error: (error) => { console.log(error) }
+      error: (error) => {
+         console.log(error);
+         this.toastr.error(error.error);
+        }
     })
   };
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 
@@ -12,7 +13,8 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +24,10 @@ export class RegisterComponent implements OnInit {
         next: (response) => {
           console.log(response)
           this.cancel()},
-        error: (error) => console.log(error)
+        error: (error) => {
+          console.log(error);
+          this.toastr.error(error.error);
+        }
     })
   }
 
